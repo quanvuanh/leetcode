24495ceabe7413646c64df7255e399da88d1e94c
Quan Vu Anh <quan.vuanh52@gmail.com>
2025-04-23 12:47:21 +0200

    small adjustment to word frequency count

--- problems/src/countfrequency/Solution.java
+++ problems/src/countfrequency/Solution.java
@@ -5,10 +5,12 @@ import java.util.Map;
 
 public class Solution {
   public static void main(String[] args) {
-
+    Map<String, Integer> a = Solution.countFrequency("This is a text with some word repetitions hi hello is what who he is like that or when he is are we like that okay");
+    System.out.println(a);
   }
 
-  public static Map<String, Integer> countFrequency(String[] words) {
+  public static Map<String, Integer> countFrequency(String text) {
+    String[] words = text.split(" ");
     Map<String, Integer> frequency = new HashMap<>();
     for (String word : words) {
       frequency.put(
